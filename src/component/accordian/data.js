05dd54f0d7ec2921e76db1635c0320389e87adb6
Retep-dev <afolabiengopet@gmail.com>
2024-12-11 21:52:37 +0100

    second Commit

---
+++ src/component/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id : '1',
+        question: "what are accordion components?",
+        answer: "Accordion components are the parts that make up an accordion, which is a vertical stack of interactive headings that users can click to expand or collapse content."
+    },
+    {
+        id : '2',
+        question: "what are they used for?",
+        answer: "Accordion components are the parts that make up an accordion, which is a vertical stack of interactive headings that users can click to expand or collapse content."
+    },
+    {
+        id : '3',
+        question: "Accordion as a musical instrument?",
+        answer: "Accordion components are the parts that make up an accordion, which is a vertical stack of interactive headings that users can click to expand or collapse content."
+    },
+    {
+        id : '4',
+        question: "Can I create an aacordion?",
+        answer: "Accordion components are the parts that make up an accordion, which is a vertical stack of interactive headings that users can click to expand or collapse content."
+    },
+ ];
+
+ export default data;
